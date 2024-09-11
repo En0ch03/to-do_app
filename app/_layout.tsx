b091eb8d70e1e16fbb08c12169c4bc7a12807328
Enoch <yusufemredogan.2003@gmail.com>
2024-09-11 16:17:56 +0300

    second

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,19 @@
 import { Stack } from "expo-router";
 import { View, StyleSheet } from "react-native";
-import styles from "@/src/components/input/style";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerTitle: "My To-do App" }}></Stack>;
+  return (
+    <Stack
+      screenOptions={{
+        headerTitle: "My To-do App",
+        headerStyle: { backgroundColor: "#00CCDD" },
+        headerTintColor: "#F5F5F5",
+        headerTitleStyle: {
+          fontWeight: "bold",
+          fontFamily: "Georgia",
+          fontSize: 20,
+        },
+      }}
+    ></Stack>
+  );
 }
